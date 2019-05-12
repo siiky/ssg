45114b3d582885f98ef502efbd24d65a52977c5f
siiky <siiky@users.noreply.github.com>
2019-05-12 16:12:54 +0100

    MD -> HTML

--- ssg.scm
+++ ssg.scm
@@ -1,13 +1,15 @@
 (import chicken.base)
 (import chicken.file)
+(import chicken.io)
 (import chicken.irregex)
 (import chicken.pathname)
 (import chicken.process-context)
+(import lowdown)
 (import optimism)
 (import srfi-1)
 (import srfi-13)
-(import typed-records)
 (import sxml-transforms)
+(import typed-records)
 
 ; http://www.more-magic.net/docs/scheme/sxslt.pdf
 
@@ -35,24 +37,24 @@
 
   (define (*text* _ str) str)
   (define (*default* . x) x)
-
   (define (h1 _ title) `(h1 "# " ,title "\n"))
   (define (h2 _ title) `(h2 "## " ,title "\n"))
   (define (h3 _ title) `(h3 "### " ,title "\n"))
   (define (h4 _ title) `(h4 "#### " ,title "\n"))
   (define (h5 _ title) `(h5 "##### " ,title "\n"))
-
-  (define (l _ ref)
-    `(a (@ (href ,ref)) ,ref "\n"))
-
-  (define (toc _ . entries)
-    `(ul ,entries))
+  (define (p _ . content) content)
+  (define (l _ ref) `(a (@ (href ,ref)) ,ref "\n"))
+  (define (toc _ . entries) `(ul ,entries))
+  ; TODO: Multi line code
+  (define (code _ . content) `("`" ,@content "`"))
 
   `((h1 . ,h1)
     (h2 . ,h2)
     (h3 . ,h3)
     (h4 . ,h4)
     (h5 . ,h5)
+    (code . ,code)
+    (p . ,p)
     (l . ,l)
     (toc . ,toc)
     (page . ,page)
@@ -60,11 +62,13 @@
     (*default* . ,*default*)))
 
 (define (main args)
-  (with-output-to-file
-    "test.html"
-    (lambda ()
-      (SRV:send-reply
-        (pre-post-order (pre-post-order sem-page (custom-rules))
-                        universal-conversion-rules)))))
+  (let ((tree `(page "./test.md" ,(markdown->sxml (with-input-from-file (car args) read-string)))))
+    (with-output-to-file
+      "test.html"
+      (lambda ()
+        (SRV:send-reply
+          (pre-post-order (pre-post-order tree (custom-rules))
+                          universal-conversion-rules))
+        (newline)))))
 
 (main (command-line-arguments))
