5e6a37916772b8d8c61e21a5b1763aef95960ffd
siiky <siiky@users.noreply.github.com>
2020-07-31 17:04:56 +0100

    Filter also WIP directories

--- ssg.site.scm
+++ ssg.site.scm
@@ -75,7 +75,7 @@
     (filter identity
             (index-map-all-entries
               (lambda (dir ent)
-                (and (not (ent-wip? ent))
+                (and (not (or (dir-wip? dir) (ent-wip? ent)))
                      (feed-entry (ent-title ent)
                                  (relative-path (dir-name dir)
                                                 (ent-name ent)))))
